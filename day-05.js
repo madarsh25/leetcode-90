d24be31c9e181b09cec804ee6a891b78bf034053
adarsh <adarsh.jy@gmail.com>
2026-01-25 21:12:02 +0530

    Day 5: Best time to buy and sell stocks

---
+++ day-05.js
@@ -0,0 +1,13 @@
+// 121- Best time to buy and sell stocks
+
+var maxProfit = function (prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+  }
+
+  return maxProfit;
+};
