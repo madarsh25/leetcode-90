c3e5dac314268cd4491786a5036f09ec1fd15444
adarsh <adarsh.jy@gmail.com>
2026-02-13 22:30:33 +0530

    Day 24: Longest Substring

---
+++ day-24.js
@@ -0,0 +1,17 @@
+// 3. Longest Substring Without Repeating Characters
+
+var lengthOfLongestSubstring = function (s) {
+  let set = new Set();
+  let l = 0;
+  let max = 0;
+
+  for (let r = 0; r < s.length; r++) {
+    while (set.has(s[r])) {
+      set.delete(s[l]);
+      l++;
+    }
+    set.add(s[r]);
+    max = Math.max(max, r - l + 1);
+  }
+  return max;
+};
