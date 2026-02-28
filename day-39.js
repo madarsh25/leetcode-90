809c3c9f37eb6a84368daafe31e77cd3789d2e3b
adarsh <adarsh.jy@gmail.com>
2026-03-01 00:16:52 +0530

    Day 39: Frequent Elements

---
+++ day-39.js
@@ -0,0 +1,13 @@
+// 347. Top K Frequent Elements
+
+var topKFrequent = function (nums, k) {
+  const count = {};
+  for (const num of nums) {
+    count[num] = (count[num] || 0) + 1;
+  }
+
+  const arr = Object.entries(count).map(([num, freq]) => [freq, parseInt(num)]);
+  arr.sort((a, b) => b[0] - a[0]);
+
+  return arr.slice(0, k).map((pair) => pair[1]);
+};
