0e7539f364544d37bbfc539f3cbd125bdf283df1
adarsh <adarsh.jy@gmail.com>
2026-03-08 23:54:42 +0530

    Day 47: 132 pattern

---
+++ day-47.js
@@ -0,0 +1,18 @@
+// 456. 132 Pattern
+
+var find132pattern = function (nums) {
+  let stack = [];
+  let third = -Infinity;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] < third) return true;
+
+    while (stack.length && nums[i] > stack[stack.length - 1]) {
+      third = stack.pop();
+    }
+
+    stack.push(nums[i]);
+  }
+
+  return false;
+};
