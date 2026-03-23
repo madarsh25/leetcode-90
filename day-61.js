e3b22675699c107c1285863fcad721a219a293ca
adarsh <adarsh.jy@gmail.com>
2026-03-23 22:45:00 +0530

    Day 61: Factorial Trailing Zeros
    Implement function to calculate trailing zeroes in factorial.

---
+++ day-61.js
@@ -0,0 +1,13 @@
+// 172. Factorial Trailing Zeroes
+
+var trailingZeroes = function (n) {
+    let count = 0;
+
+    while (n > 0) {
+        n = Math.floor(n / 5);
+        count += n;
+    }
+
+    return count;
+
+};
