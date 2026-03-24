5ef5ebea56ad6880aeb1ea904e982b7ee6d51e4b
adarsh <adarsh.jy@gmail.com>
2026-03-24 19:29:05 +0530

    Day 63: K Closest Elements

---
+++ day-63.js
@@ -0,0 +1,18 @@
+// 658. Find K Closest Elements
+
+var findClosestElements = function(arr, k, x) {
+    let left = 0;
+    let right = arr.length - k;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (x - arr[mid] > arr[mid + k] - x) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return arr.slice(left, left + k);
+};
