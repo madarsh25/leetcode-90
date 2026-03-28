3ff8cba41a6eb269a2a8b542aa9eb0bf13b38673
adarsh <adarsh.jy@gmail.com>
2026-03-29 00:11:53 +0530

    Day 67: Multiply Strings

---
+++ day-67.js
@@ -0,0 +1,22 @@
+// 43. Multiply Strings
+
+var multiply = function(num1, num2) {
+    if (num1 === "0" || num2 === "0") return "0";
+
+    let m = num1.length;
+    let n = num2.length;
+    let res = new Array(m + n).fill(0);
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            let mul = (num1[i] - '0') * (num2[j] - '0');
+
+            let sum = mul + res[i + j + 1];
+
+            res[i + j + 1] = sum % 10;      
+            res[i + j] += Math.floor(sum / 10); 
+        }
+    }
+
+    return res.join('').replace(/^0+/, '');
+};
