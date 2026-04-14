112d1e25ed875f8e9ec5cc3c1d0116b509ed84c0
adarsh <adarsh.jy@gmail.com>
2026-04-14 23:52:08 +0530

    Day 84: Sqrt(x)

---
+++ day-84.js
@@ -0,0 +1,18 @@
+// 69. Sqrt(x)
+
+var mySqrt = function(x) {
+    if (x < 2) return x;
+
+    let left = 1, right = Math.floor(x / 2);
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+
+        if (square === x) return mid;
+        if (square < x) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return right;
+};
